5ad6aa55d642f247e48292f78feba9ba12c046bb
zavizion-koober <aleshkazavizion@gmail.com>
2026-03-02 18:24:38 +0400

    feat: add new migration for ArcanaEntity to include EnergyState owned type

--- DestinyMatrix/obj/Debug/net8.0/DestinyMatrix.AssemblyInfo.cs
+++ DestinyMatrix/obj/Debug/net8.0/DestinyMatrix.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DestinyMatrix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126339a67a64b27216f08f74134e9731bd5919fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c84f86299e71a27d3703b93e4d29888fa8e6ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("DestinyMatrix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DestinyMatrix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
